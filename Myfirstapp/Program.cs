6dc1b3f2b69b00384585c56b81f90a29f227f0bc
Kertu Ali <kertu.greenberg@tktk.ee>
2023-09-07 17:56:09 +0200

    Create new app

---
+++ Myfirstapp/Program.cs
@@ -0,0 +1,13 @@
+﻿// This is a comment to my code
+
+//rakendus kysib kasutajalt sisestada tema nime
+//rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name"); //Output
+// string- s6ne
+string userName = Console.ReadLine(); //Input
+
+Console.WriteLine("Hello" +", " + userName + "!"); //Output
+
+//string interpolation
+Console.WriteLine($"Hello, {userName}!"); //Output
